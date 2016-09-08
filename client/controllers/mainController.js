9c995444140e8bc36932d1e17232ecc7f424a626
ChrisHeo <christopher.s.heo@gmail.com>
2016-09-08 15:14:10 -0400

    (feat) display number of solutions

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -9,17 +9,20 @@
 
   function mainController($scope) {
     $scope.queenPuzzle = queenPuzzle;
+    $scope.solutionNum;
     $scope.addQueen = addQueen;
     $scope.hasConflict = hasConflict;
     $scope.updateBoard = updateBoard;
     $scope.createBoard = createBoard;
-    $scope.data = {
+    $scope.boardOption = {
       availableOptions: [
           {id: '1', name: '8 x 8', value: 8},
           {id: '2', name: '7 x 7', value: 7},
           {id: '3', name: '6 x 6', value: 6},
           {id: '4', name: '5 x 5', value: 5},
-          {id: '5', name: '4 x 4', value: 4}],
+          {id: '5', name: '4 x 4', value: 4},
+          {id: '6', name: '3 x 3', value: 3},
+          {id: '7', name: '2 x 2', value: 2}],
       selectedOption:
           {id: '1', value: 8}
     };
@@ -43,16 +46,14 @@
 
     function updateBoard(num) {
       var board1 = ChessBoard('board1', cfg, num.value);
-      queenPuzzle(num.value, num.value);
+      var answer = queenPuzzle(num.value, num.value);
+      $scope.solutionNum = answer !== []? answer.length : 0;
     }
 
     function queenPuzzle(rows, columns) {
       if (rows <= 0) {
-        return [
-          []
-        ];
+        return [[]];
       } else {
-        console.log(addQueen(rows -1,columns));
         return addQueen(rows - 1, columns);
       }
     }
@@ -67,26 +68,26 @@
             newSolutions.push(solution.concat([newColumn]));
         }
       }
-      if(newSolutions[0].length === columns) {
+//       if( newSolutions[0].length === columns) {
 
-        var alphabet = ['a','b','c','d','e','f','g','h'];
-        var holder = {};
+//         var alphabet = ['a','b','c','d','e','f','g','h'];
+//         var holder = {};
 
-        for(var i = 0; i < columns; i++) {
+//         for(var i = 0; i < columns; i++) {
 
-          holder[alphabet[i] + newSolutions[0][i]] =  "wQ"
-          console.log(holder)
+//           holder[alphabet[i] + newSolutions[0][i]] =  "wQ"
+//           console.log(holder)
 
-        }
-        console.log(holder)
+//         }
+//         console.log(holder)
 
-// {a1:"wQ",b3:"wQ",c0:"wQ",d2:"wQ",}
+// // {a1:"wQ",b3:"wQ",c0:"wQ",d2:"wQ",}
 
 
-      cfg.position = holder;
-      console.log(cfg)
-      createBoard(columns)
-      }
+//       cfg.position = holder;
+//       console.log(cfg)
+//       createBoard(columns)
+//       }
 
       console.log(newSolutions);
       return newSolutions;
