83fd78be752f01b7b2ff9e80302685653ffbb787
ChrisHeo <christopher.s.heo@gmail.com>
2016-09-09 12:41:59 -0400

    (feat) backend api ready to connect to frontend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
+var Solve = require('./solvers.js');
 
 var app = express();
 
@@ -8,8 +9,20 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../client/'));
 
+app.post('/api/solve', function (req, res){
+  console.log(req.body);
+  if( (req.body.chessPiece === 'q' || req.body.chessPiece === 'r') &&
+    req.body.n >=2 && req.body.n <= 8){
+    res.status(200).send(Solve.solve(req.body.chessPiece, req.body.n));
+  } else {
+    res.status(400).send('Solutions are not available');
+  }
+});
+
 var port = process.env.PORT || 9000;
 
 app.listen(port, function() {
   console.log('listening on port ', port) ;
 });
+
+module.exports = app;
---
+++ server/solvers.js
@@ -0,0 +1,57 @@
+(function() {
+
+  var piece;
+
+  function puzzle(rows, columns){
+    if (rows <= 0) {
+      return [
+        []
+      ];
+    } else {
+      return addQueenOrRook(rows - 1, columns);
+    }
+  }
+
+  function addQueenOrRook (newRow, columns, prevSolution){
+    var newSolutions = [];
+      var prev = puzzle(newRow, columns);
+      for (var i = 0; i < prev.length; i++) {
+        var solution = prev[i];
+        for (var newColumn = 0; newColumn < columns; newColumn++) {
+          if (!hasConflict(newRow, newColumn, solution))
+            newSolutions.push(solution.concat([newColumn]));
+        }
+      }
+      return newSolutions;
+  }
+
+  function hasConflict(newRow, newColumn, solution) {
+    for (var i = 0; i < newRow; i++) {
+      if (piece === 'q') {
+        if (solution[i] == newColumn ||
+          solution[i] + i == newColumn + newRow ||
+          solution[i] - i == newColumn - newRow) {
+          return true;
+        }
+      } else {
+        if (
+          solution[i] == newColumn
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  exports.solve = function(chessPiece, n) {
+    piece = chessPiece;
+    var solutions = puzzle(n, n);
+    var obj = {
+      solutions: solutions,
+      solutionNum: solutions.length
+    }
+    return obj;
+  }
+
+})();
