828031f26a4f25f4291155650a4dc5ce7c5fb8ee
Jeff Lam <jefflam1200@gmail.com>
2016-09-08 20:24:41 -0700

    (feature) add solution loop for n rooks

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -25,22 +25,23 @@
           {id: '5', name: '4 x 4', value: 4},
           {id: '6', name: '3 x 3', value: 3},
           {id: '7', name: '2 x 2', value: 2}],
-      selectedOption:
-          {id: '1', value: 8}
+      selectedOption: {
+        id: '1',
+        value: 8
+      }
     };
 
     var cfg = {
       position: {
-        // d4: 'wR',
-        // d1: 'bR',
-        // e4: 'wQ'
       }
     };
+
+    var piece = "";
 
 
     function createBoard(n) {
       var number = n;
-      if(number === undefined) {
+      if (number === undefined) {
         number = 8;
       }
       $scope.solutionNum = undefined;
@@ -57,42 +58,53 @@
       clearTimeout(window.loop);
     }
 
+
+
     function updateBoard(num, chessPiece) {
-      if(chessPiece !== 'q' &&  chessPiece !== 'r') {
+      piece = chessPiece;
+
+      if (chessPiece !== 'q' && chessPiece !== 'r') {
         console.log('error with selected chess piece:', chessPiece);
         return;
       }
 
-      function solutionLoop (x, boardSize){
-        window.loop = setTimeout(function(){
-                        createBoard();
-                        answer[x].forEach(function(positionValue, index){
-                          var coordinate = alphabet[index] + (boardSize - positionValue);
-                          cfg.position[coordinate] = 'wQ';
-                        });
-                        board1 = ChessBoard('board1', cfg, boardSize);
-                        $scope.solutionNum = x + 1; console.log($scope.solutionNum);
-                        if(++x < answer.length) solutionLoop(x, boardSize);
-                      }, 1500);
+      function solutionLoop(x, boardSize) {
+        window.loop = setTimeout(function() {
+          createBoard();
+          answer[x].forEach(function(positionValue, index) {
+            var coordinate = alphabet[index] + (boardSize - positionValue);
+            if (chessPiece === 'q') {
+              cfg.position[coordinate] = 'wQ';
+            }
+            if (chessPiece === 'r') {
+              cfg.position[coordinate] = 'wR';
+            }
+          });
+          board1 = ChessBoard('board1', cfg, boardSize);
+          $scope.solutionNum = x + 1;
+          if (++x < answer.length) solutionLoop(x, boardSize);
+        }, 1500);
       }
 
       var board1 = ChessBoard('board1', cfg, num.value);
       var answer;
-      var alphabet = ['a','b','c','d','e','f','g','h'];
+      var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-      if(chessPiece === 'q') answer = queenPuzzle(num.value, num.value);
-      else if(chessPiece === 'r') answer = []; //ROOK ALGO HERE
+      if (chessPiece === 'q') answer = queenPuzzle(num.value, num.value);
+      else if (chessPiece === 'r') answer = queenPuzzle(num.value, num.value);
 
-      $scope.solutions = answer.length || 'NEED TO CONNECT ROOK SOLUTION' ;
+      $scope.solutions = answer.length || "No solution available.";
 
-      if(answer.length > 0){
+      if (answer.length > 0) {
         solutionLoop(0, num.value);
       }
     }
 
     function queenPuzzle(rows, columns) {
       if (rows <= 0) {
-        return [[]];
+        return [
+          []
+        ];
       } else {
         return addQueen(rows - 1, columns);
       }
@@ -113,14 +125,21 @@
 
     function hasConflict(newRow, newColumn, solution) {
       for (var i = 0; i < newRow; i++) {
-        if (solution[i] == newColumn ||
-          solution[i] + i == newColumn + newRow ||
-          solution[i] - i == newColumn - newRow) {
-          return true;
+        if (piece === 'q') {
+          if (solution[i] == newColumn ||
+            solution[i] + i == newColumn + newRow ||
+            solution[i] - i == newColumn - newRow) {
+            return true;
+          }
+        } else {
+          if (
+            solution[i] == newColumn
+          ) {
+            return true;
+          }
         }
       }
       return false;
     }
   }
-
 })();
